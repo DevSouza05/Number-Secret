e0a1664e1112bb310004dc7ddbaba96433aee03a
peterson.moraes <peterson.moraes@PC_SUPORTE>
2023-10-19 11:27:34 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Jogo
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Digite um numero de um 1 a 10");
+            Random aleatorio = new Random();
+           
+            bool acertou = false;
+
+                int numeroSecreto = aleatorio.Next(1, 10);
+            do 
+            {
+                int chute = int.Parse(Console.ReadLine());
+
+                if (chute == numeroSecreto)
+                {
+                    Console.Write("Parabens você acertou !! ");
+                     Console.Write("O numero secreto é: " + numeroSecreto);
+
+                    acertou = true;
+                }
+                else
+                    if( chute < numeroSecreto)
+                    {
+                        Console.WriteLine(" Errou! O número é secreto é maior");
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("O número secreto é menor");
+                       
+
+                    }
+            }
+            while (!acertou);
+        }
+    }
+}
